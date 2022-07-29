87996dd9f9946963fa753d73f8c16b31e66fded7
tasha-03 <tatyana.pavl03@gmail.com>
2022-07-29 10:04:17 +0300

    feat: inspecting suites and messages

--- index.js
+++ index.js
@@ -1,28 +1,54 @@
 const express = require("express");
-const open = require("open");
 const fs = require("fs");
-
-let data = JSON.parse(
-  fs
-    .readFileSync(
-      "./data/test-results.json"
-    )
-    .toString()
-);
+const Convert = require("ansi-to-html");
+const convert = new Convert();
+const filename =
+  // "C:/Users/tasha/Documents/VS Code/Web/SplunkProxy/SplunkProxy/demo/test-results.json";
+  "data/test-results.json";
 
 const app = express();
 
 const testResults = (req, res) => {
-  const results = data;
+  let data = JSON.parse(fs.readFileSync(filename));
+  const results = data || {};
 
   res.render("index", { results });
+};
+
+const getSuite = (req, res) => {
+  let data = JSON.parse(fs.readFileSync(filename));
+  const results = data.testResults[req.params.ID] || {};
+
+  results && results.message
+    ? (results.message = convert.toHtml(results.message))
+    : null;
+
+  res.render("test-suite", { results, id: req.params.ID });
+};
+
+const getTest = (req, res) => {
+  let data = JSON.parse(fs.readFileSync(filename));
+  const test =
+    data.testResults[req.params.suiteID].assertionResults[req.params.ID];
+  test && test.failureMessages
+    ? (test.failureMessages = test.failureMessages.map((m) =>
+        convert.toHtml(m)
+      ))
+    : null;
+
+  res.render("test-results", {
+    test,
+    suiteId: req.params.suiteID,
+    id: req.params.ID,
+  });
 };
 
 app.set("view engine", "ejs");
 
 app.get("/", testResults);
+app.get("/:ID", getSuite);
+app.get("/:suiteID/:ID", getTest);
 
 let server = app.listen(8000, () => {
-  console.log("Server is online on port: 8000");
+  console.log("Server is online on port:", server.address().port);
 });
-
